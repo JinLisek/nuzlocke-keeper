53aab92de2ed215469dc6530b10c76f0635e5521
JinLisek <tastyramen@gmail.com>
2023-11-22 23:07:23 +0100

    Use generation name in generations links

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ const Home = async () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ul>
-        {generations.map((generation, idx) => (
+        {generations.map((generation) => (
           <li key={generation.name}>
-            <Link href={{ pathname: `/generations/${idx}` }}>{generation.name}</Link>
+            <Link href={{ pathname: `/generations/${generation.name}` }}>{generation.name}</Link>
           </li>
         ))}
       </ul>
